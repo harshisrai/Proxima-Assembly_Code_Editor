4280f8e050dc62d4347d05962f6d3f773a6d1088
HARSH RAI <hrai1942@gmail.com>
2025-04-19 00:24:23 +0530

    Debugged BP

--- input.asm
+++ input.asm
@@ -1,16 +1,42 @@
-t1:
-addi x1 x0 1
-jal x0  t4
-t2:
-addi x2 x0 1
-jal x0  t5
-t3:
-addi x3 x0 1
-jal x0  exit
-t4:
-addi x4 x0 1
-jal x0  t2
-t5:
-addi x5 x0 1
-jal x0  t3
+.data
+.asciz "NamananamaN"
+
+.text
+addi x31 x0 11 # size of string
+addi x10 x0 0 # initial left pointer
+lui x30 0x10000
+jal  x1 palindrome
+addi x17 x16 5
+beq x0 x0 exit
+
+palindrome:
+addi x2 x2 -4
+sw x1 0 x2
+sub x12 x31 x10
+addi x12 x12 -1 # current right pointer
+bge x10 x12 true
+add x13 x30 x10
+lb x14 0 x13
+add x15 x30 x12
+lb x16 0 x15
+bne x16 x14 false
+addi x10 x10 1
+jal x1 palindrome
+lw x1 0 x2
+addi x2 x2 4
+jalr x0 x1 0
+
+
+true:
+addi x20 x0 1
+lw x1 0 x2
+addi x2 x2 4
+jalr x0 x1 0
+
+false:
+addi x20 x20 0
+lw x1 0 x2
+addi x2 x2 4
+jalr x0 x1 0
+
 exit:
